f71cab66123a5d305e46105fb9bb912f9ae72cd4
willnaughton <banjo.ninja123@gmail.com>
2018-12-19 13:21:36 -0700

    adding switch with jquery

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 $(document).ready(function() {
-    $('button').click(function() {
-        $('div').text(.switchContent);
-    })
+    var intro = $("#intro");
+    var learningObj = $("#learningObj");
+    var prereqs = $("#prereqs");
+    var requirements = $("#requirements");
 
+        intro.show();
+        learningObj.hide();
+        prereqs.hide();
+        requirements.hide();
 
+    function addIntro(){
+        intro.show();
+        learningObj.hide();
+        prereqs.hide();
+        requirements.hide();
+    }
+    $("#introB").click(addIntro);
 
+    function addlearningObj(){
+        intro.hide();
+        learningObj.show();
+        prereqs.hide();
+        requirements.hide();
+    }
+    $("#learningObjB").click(addlearningObj);
+
+    function addPrereqs(){
+        intro.hide();
+        learningObj.hide();
+        prereqs.show();
+        requirements.hide();
+    }
+    $("#prereqsB").click(addPrereqs);
+
+    function addRequirements(){
+        intro.hide();
+        learningObj.hide();
+        prereqs.hide();
+        requirements.show();
+    }
+    $("#reqsB").click(addRequirements);
 });
