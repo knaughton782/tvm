d96be58f099fe3aed2c19888866eafe894a020cb
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-01-08 09:41:00 -0700

    switching slides continue and back jquery added to js file

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 $(document).ready(function() {
 
-    var continueBtn = $("#continue");
-    function chgSlides() {
-        $("#slide1").hide();
-        $("#slide2").show();
+/****************** BACK / CONTINUE BUTTON FUNCTIONALITY ********************/
 
-    }
-    $(continueBtn).click(chgSlides);
+    // hide all slides except first slide
+    $(".allSlides .slide").each(function(e) {
+        if (e !=0) {
+            $(this).hide();
+        }
+    });
+
+    // for each click on the continue button, hide the current slide and show the next one
+    $(".continueBtn").click(function() {
+        if ($(".allSlides .slide:visible").next().length != 0) {
+            $(".allSlides .slide:visible").next().show().prev().hide();
+        } else {
+            $(".allSlides .slide:visible").hide();
+            $(".allSlides .slide:first").show();
+        }
+        return false;
+    });
+
+    // for each click on the back button, hide the current slide and show the previous one
+    $(".backBtn").click(function() {
+        if ($(".allSlides .slide:visible").prev().length !=0) {
+            $(".allSlides .slide:visible").prev().show().next().hide();
+        } else {
+            $(".allSlides .slide:visible").hide();
+            $(".allSlides .slide:last").show();
+        }
+        return false;
+    });
     
+/****************** END Back / Continue Button Functionality ********************/
+    
+        
     
     
     
